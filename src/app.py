62bdc98634d03c8b1ff413b9b4fd248f3c369ee8
Audrey Holmes <auholmes@uw.edu>
2019-04-14 22:32:18 +0000

    basic style

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def submit_text():
+    text = request.form['text']
+    return 'Sorry, this doesn\'t do anything (yet).'
 
 if __name__ == '__main__':
     WSGIServer(('0.0.0.0', 3000), app).serve_forever()

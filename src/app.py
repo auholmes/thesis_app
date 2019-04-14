2136e5bae8acd559d6286fc27246b67e253569bb
Audrey Holmes <auholmes@uw.edu>
2019-04-14 20:05:43 +0000

    input text box

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hi'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=False)
+    WSGIServer(('0.0.0.0', 3000), app).serve_forever()

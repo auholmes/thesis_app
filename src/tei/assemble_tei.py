021ba141727c57b8e939c81522b561c503740e1c
Audrey Holmes <auholmes@uw.edu>
2019-05-22 00:29:23 -0400

    fix pretty print

--- src/tei/assemble_tei.py
+++ src/tei/assemble_tei.py
@@ -1,8 +1,10 @@
+import re
 from bs4 import BeautifulSoup
+from lxml import etree
 
 
 def create_header(title='', author='', editor=''):
-    soup = BeautifulSoup(features='xml')
+    soup = BeautifulSoup()
     soup.append(soup.new_tag('teiHeader'))
     soup.find('teiHeader').append(soup.new_tag('fileDesc'))
     soup.find('fileDesc').append(soup.new_tag('titleStmt'))
@@ -19,10 +21,11 @@ def create_header(title='', author='', editor=''):
 
 
 def create_xml(header, body=''):
-    soup = BeautifulSoup(features='xml')
+    soup = BeautifulSoup()
     soup.append(soup.new_tag('TEI', xmlns="http://www.tei-c.org/ns/1.0"))
     soup.TEI.append(header)
-    return soup.prettify()
+    root = etree.fromstring(str(soup))
+    return etree.tostring(root, pretty_print=True).decode()
 
 
 if __name__ == '__main__':
